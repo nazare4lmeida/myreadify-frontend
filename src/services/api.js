0f00a32e87914e37ee6173a8d7a6553fcf697372
Nazare Almeida <nazyalmeida@gmail.com>
2025-07-22 18:15:38 -0300

    correção no api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,12 @@
-// src/services/api.js
-
 import axios from 'axios';
 
-const backendUrl = 'https://myreadify-backend.vercel.app';
-
-const apiUrl = import.meta.env.PROD ? backendUrl : 'http://localhost:3333';
-
 const api = axios.create({
-  baseURL: apiUrl
+  baseURL: import.meta.env.VITE_API_URL
 });
 
-// Interceptor que insere o token JWT no cabeçalho Authorization
+// Interceptor JWT
 api.interceptors.request.use((config) => {
-
-  // A chave usada para buscar o token agora é a mesma usada no seu AuthContext.
   const token = localStorage.getItem('@MyReadify:token');
-
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
